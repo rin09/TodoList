41a4291321056fc5373a7e2ae7569bd10165d201
Suzuki Rintaro <r09.dr23@gmail.com>
2018-08-12 11:44:35 +0900

    settings

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports={
+  mode: "development",
+  entry: ["./src/app.js"],
+  output: {
+    path: __dirname+"/dist",
+    filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        resource: {
+          test: /\.jsx?$/,
+          exclude: /node_modules/
+        },
+        use: ["babel-loader"]
+      }
+    ]
+  }
+}
